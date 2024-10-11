37867513e14ce7863f3531f228abda9b898799b3
devyanaby <devyanaby@gmail.com>
2024-10-11 08:50:59 +0700

    store

---
+++ config/sidebar.php
@@ -0,0 +1,140 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specify
+    | an array of paths that should be checked for your views. Of course
+    | the usual Laravel view path has already been registered for you.
+    |
+    */
+ 'menu' => [[
+		'icon' => 'fa fa-sitemap',
+		'title' => 'Dashboard',
+		'url' => 'javascript:;',
+			'url' => '/dashboard',
+			'title' => 'Dashboard',
+			'route-name' => 'dashboard'
+	],[
+		'icon' => 'fa fa-utensils',
+		'title' => 'POS System',
+		'url' => '/pos/customer-order',
+	],[
+	'icon' => 'fa fa-database',
+		'title' => 'MASTER',
+		'url' => 'javascript:;',
+		'caret' => true,
+		'sub_menu' => [[
+			'url' => '/kategori',
+			'title' => 'Kategori',
+			'route-name' => 'index'
+		],[
+			'url' => '',
+			'title' => 'Produk',
+			'route-name' => ''
+		],[
+			'url' => '',
+			'title' => 'Supplier',
+			'route-name' => ''
+				
+		]]
+	],[
+	'icon' => 'fa fa-money-bill',
+		'title' => 'TRANSAKSI',
+		'url' => 'javascript:;',
+		'caret' => true,
+		'sub_menu' => [[
+			'url' => '/menu',
+			'title' => 'Pengeluaran',
+			'route-name' => '/menu'
+		],[
+			'url' => '/',
+			'title' => 'Pembelian',
+			'route-name' => ''
+		],[
+			'url' => '/',
+			'title' => 'Penjualan',
+			'route-name' => ''
+		],[
+			'url' => '/',
+			'title' => 'Transaksi lama',
+			'route-name' => ''
+		],[
+			'url' => '/',
+			'title' => 'Transaksi baru',
+			'route-name' => ''
+						
+		]]
+	
+	],[
+		'icon' => 'fa fa-file',
+		'title' => 'REPORT',
+		'url' => 'javascript:;',
+		'caret' => true,
+		'sub_menu' => [[
+				'url' => '/laporan/transaksi',
+				'title' => 'Transaksi',
+				'route-name' => 'laporan-transaksi'
+			],[
+				'url' => '/laporan/pengeluaran',	
+				'title' => 'Pengeluaran',
+				'route-name' => 'laporan-pengeluaran'
+			],[
+				'url' => '/laporan/pemasukan',
+				'title' => 'Pemasukan',
+				'route-name' => 'laporan-pemasukan'
+			],[
+				'url' => '/laporan/penjualan',
+				'title' => 'Penjualan',
+				'route-name' => 'laporan-penjualan'
+			]]		
+	],[
+	'icon' => 'fa fa-gift',
+		'title' => 'Manage Menu',
+		'url' => 'javascript:;',
+		'caret' => true,
+		'sub_menu' => [[
+			'url' => '/menu',
+			'title' => 'Daftar Menu',
+			'route-name' => '/menu'
+		],[
+			'url' => '/',
+			'title' => 'Daftar Kategori',
+			'route-name' => ''
+		]]		
+	],[
+		'icon' => 'fa fa-users',
+			'title' => 'User',
+			'url' => 'javascript:;',
+			'route-name' => '/pengaturan'
+	],[
+		'icon' => 'fa fa-cog',
+			'title' => 'Pengaturan',
+			'url' => 'javascript:;',
+			'route-name' => '/pengaturan'
+	
+	],[
+		'icon' => 'fa fa-key',
+		'title' => 'LRL',
+		'url' => 'javascript:;',
+		'caret' => true,
+		'sub_menu' => [[
+			'url' => '/login',
+			'title' => 'Login',
+			'route-name' => 'login'
+		],[
+			'url' => '/register',
+			'title' => 'Register',
+			'route-name' => 'register'
+		],[
+			'url' => '/login',
+			'title' => 'Logout',
+			'route-name' => 'login'
+		]]
+
+	]]
+];
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+use App\Http\Controllers\KategoriController;
+
+
+
+Route::get('/', function () {
+	return redirect('/dashboard');
+});
+Route::get('/dashboard', 'MainController@dashboard')->name('dashboard');
+
+Route::get('/kategori/data', [KategoriController::class, 'data']);
+Route::resource('kategori' , KategoriController::class);
+
+// Route::get('kategori/index', 'MainController@kategori')->name('kategori');
+
+// Route::get('/laporan/penjualan', 'MainController@laporanPenjualan')->name('laporan-penjualan');
+// Route::get('/laporan/transaksi', 'MainController@laporanTransaksi')->name('laporan-transaksi');
+// Route::get('/laporan/pemasukan', 'MainController@laporanPemasukan')->name('laporan-pemasukan');
+// Route::get('/laporan/pengeluaran', 'MainController@laporanPengeluaran')->name('laporan-pengeluaran');
+
+// Route::get('/pos/customer-order', 'MainController@posCustomerOrder')->name('pos-customer-order');
+// Route::get('/pos/kitchen-order', 'MainController@posKitchenOrder')->name('pos-kitchen-order');
+// Route::get('/pos/counter-checkout', 'MainController@posCounterCheckout')->name('pos-counter-checkout');
+// Route::get('/pos/table-booking', 'MainController@posTableBooking')->name('pos-table-booking');
+// Route::get('/pos/menu-stock', 'MainController@posMenuStock')->name('pos-menu-stock');
+
+
+// Route::get('/extra/invoice', 'MainController@extraInvoice')->name('extra-invoice');
+// Route::get('/extra/profile', 'MainController@extraProfile')->name('extra-profile');
+
+// Route::get('/login/v1', 'MainController@loginV1')->name('login-v1');
+// Route::get('/login', 'MainController@loginV2')->name('login');
+// Route::get('/login/v3', 'MainController@loginV3')->name('login-v3');
+// Route::get('/register', 'MainController@registerV3')->name('register');
+
+
+// Route::get('/pengaturan', 'MainController@pengaturan')->name('pengaturan');
+
+// Route::get('/profil', 'MainController@profil')->name('profil');
+// Route::get('/edit/profil', 'MainController@editProfil')->name('edit-profil');
+
+// Route::get('/menu/tambah', 'MainController@menuTambah')->name('menu-tambah');
+// Route::get('/menu/daftar', 'MainController@menuDaftar')->name('menu-daftar');
+// Route::get('/menu/kategori', 'MainController@menuKategori')->name('menu-kategori');
+// Route::get('/menu/kategori/tambah', 'MainController@menuKategoriTambah')->name('menu-kategori-tambah');
+
+// Route::get('/menu', 'MainController@showMenu')->name('menu.index');
